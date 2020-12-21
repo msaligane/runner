f32004e6757d7e60ccb9631264fbeabffc8d61b1
Adam Olech <aolech@antmicro.com>
2020-12-21 12:02:52 +0000

    Update run_image.sh

--- virt/run_image.sh
+++ virt/run_image.sh
@@ -2,6 +2,22 @@
 
 set -e
 
+cd $(dirname $0)
+
+WORKDIR=$(realpath work)
+OVERLAY_IMG=$WORKDIR/overlay.img
+SIF_FILE=$WORKDIR/debian10.sif
+FREE_SPACE=$(df -B1 . | tail -n +2 | awk '{ print $4 "\t" }')
+OVERLAY_SIZE=$((FREE_SPACE / 2))
+DUMMY_DISK=$WORKDIR/small.img
+SSH_PUB_KEY=$HOME/.ssh/id_rsa
+RANDOM_MAC=$(printf '00-60-2F-%02X-%02X-%02X\n' $((RANDOM%256)) $((RANDOM%256)) $((RANDOM%256)))
+SHARE_PATH=$(realpath ../_layout/_work/)
+
+Q=$WORKDIR/qemu
+SIN=$Q.in
+SOUT=$Q.out
+
 readUntilString() {
 	echo "Reading serial until: $1"
 	while read line; do
@@ -10,17 +26,13 @@ readUntilString() {
 			echo "Not reading serial anymore."
 			break;
 		fi
-	done < /tmp/qemu.out
+	done < $SOUT
 }
 
-OVERLAY_IMG=overlay.img
-SIF_FILE=debian10.sif
-FREE_SPACE=$(df -B1 . | tail -n +2 | awk '{ print $4 "\t" }')
-OVERLAY_SIZE=$((FREE_SPACE / 2))
-DUMMY_DISK=/tmp/small.img
-SSH_PUB_KEY=$HOME/.ssh/id_rsa
-RANDOM_MAC=$(printf '00-60-2F-%02X-%02X-%02X\n' $((RANDOM%256)) $((RANDOM%256)) $((RANDOM%256)))
-SHARE_PATH=$HOME/github-actions-runner/_layout/_work
+writeSer() {
+	echo $1 > $SIN
+	sleep 0.2
+}
 
 if [ -f "$OVERLAY_IMG" ]; then
 	rm -rf $OVERLAY_IMG
@@ -34,12 +46,14 @@ if [ ! -f "$DUMMY_DISK" ]; then
 	fallocate -l 1MB $DUMMY_DISK
 fi
 
+mkdir -p $SHARE_PATH
+
 fallocate -l $OVERLAY_SIZE $OVERLAY_IMG
 
-mkfifo /tmp/qemu.in /tmp/qemu.out || true
+mkfifo $SIN $SOUT || true
 
 qemu-system-x86_64 \
-	-kernel bzImage-2020-12-17--14-22-23 \
+	-kernel $WORKDIR/bzImage-2020-12-17--14-22-23 \
 	-m 4G -append "console=ttyS0" -enable-kvm -smp $(nproc) -cpu host \
 	-drive format=raw,file=$SIF_FILE \
 	-drive format=raw,file.filename=$DUMMY_DISK,file.locking=off,file.driver=file \
@@ -53,8 +67,8 @@ qemu-system-x86_64 \
 	-smbios type=11,value="inject_key scalerunner:'$(cat ${SSH_PUB_KEY}.pub)'" \
 	-fsdev local,id=share_dev,path=$SHARE_PATH,security_model=none \
 	-device virtio-9p-pci,fsdev=share_dev,mount_tag=share_mount \
-	-serial pipe:/tmp/qemu \
-	-pidfile /tmp/qemu.pid \
+	-serial pipe:$Q \
+	-pidfile $Q.pid \
 	-display none \
 	-daemonize \
 	--enable-kvm
@@ -62,14 +76,15 @@ qemu-system-x86_64 \
 
 readUntilString "Welcome to Buildroot"
 
-sleep 0.2
-echo "scalerunner" > /tmp/qemu.in
-sleep 0.2
-echo "scalerunner" > /tmp/qemu.in
-sleep 0.2
-echo "sudo bash -c \"mke2fs /dev/sdd;mount /dev/sdd /mnt;mkdir /9p;mount -t 9p -o trans=virtio,version=9p2000.L share_mount /9p\"" > /tmp/qemu.in
-sleep 0.2
-echo "sudo singularity instance start --overlay /mnt /tmp/container.sif i" > /tmp/qemu.in
+writeSer "scalerunner"
+writeSer "scalerunner"
+writeSer "sudo bash -c \"mke2fs /dev/sdd;mount /dev/sdd /mnt;mkdir /9p;mount -t 9p -o trans=virtio,version=9p2000.L share_mount /9p\""
+writeSer "sudo singularity instance start -C --overlay /mnt --bind /9p /tmp/container.sif i"
 
 readUntilString "instance started successfully"
+
+writeSer "exit"
+
+#sleep 0.2
+#echo ifconfig $(ifconfig | head -1 | cut -f 1 -d ":" | cut -f 1 -d " ") | grep inet | head -1 | tr -s " " | cut -f 2 -d ":" | cut -f 1 -d " " | sudo tee /9p/_temp/ip
 
