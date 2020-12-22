1f00d41d40e328a87935d484b11e4ff5b31044d9
Adam Olech <aolech@antmicro.com>
2020-12-22 13:44:04 +0000

    tap.sh: harden iptables and remove ifconfig

--- virt/tap.sh
+++ virt/tap.sh
@@ -3,14 +3,14 @@
 TAP=tap0
 WAN=$(/sbin/route -n | grep ^0.0.0.0 | awk '{ print $8 "\t" }')
 GW=172.17.0.1
-C='-m comment --comment "qemu"'
+C='-m comment --comment qemu'
 RANGE=172.17.0.2,172.17.0.100
 WAN_IP=$(ip -f inet addr show $WAN | sed -En -e 's/.*inet ([0-9.]+).*/\1/p')
 RUNNING_USER=$(whoami)
 
 _term() {
 	echo "Caught signal!"
-	iptables-save | grep -v "qemu" | iptables-restore
+	/sbin/iptables-save | grep -v "qemu" | /sbin/iptables-restore
 	kill $(cat /var/run/dnsmasq-qemu.pid)
 	ip link delete $TAP
 	echo "Killing $!"
@@ -27,13 +27,14 @@ echo "wan ip:		$WAN_IP"
 echo "running as:	$RUNNING_USER"
 
 /usr/bin/ip tuntap add dev $TAP mode tap
-/sbin/ifconfig $TAP $GW up
+/usr/bin/ip a a $GW/24 dev $TAP
+/usr/bin/ip link set dev $TAP up
 
 echo 1 > /proc/sys/net/ipv4/ip_forward
 
-/sbin/iptables -t nat -A POSTROUTING -o $WAN -j MASQUERADE $C
-/sbin/iptables -I FORWARD 1 -i $TAP -j ACCEPT $C
-/sbin/iptables -I FORWARD 1 -o $TAP -m state --state RELATED,ESTABLISHED -j ACCEPT $C
+/sbin/iptables -t nat -A POSTROUTING -o $WAN -j MASQUERADE --random $C
+/sbin/iptables -A FORWARD -i $TAP -o $WAN -j ACCEPT $C
+/sbin/iptables -A FORWARD -i $WAN -o $TAP -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT $C
 
 /usr/sbin/dnsmasq \
 	--strict-order \
