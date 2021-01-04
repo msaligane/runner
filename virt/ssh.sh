a0134c9e4c99a73db94caa78ab6d5124b89fa480
Adam Olech <aolech@antmicro.com>
2021-01-04 13:12:03 +0000

    Add -s parameter to ssh helper

--- virt/ssh.sh
+++ virt/ssh.sh
@@ -4,7 +4,13 @@ IP=172.17.$1.2
 
 echo "Connecting to $IP"
 
+if [ "$2" = "-s" ]; then
+    SINGULARITY_CMD="-t bash -c 'sudo singularity shell -e instance://i'"
+else
+    SINGULARITY_CMD=""
+fi
+
 /usr/bin/ssh -q \
     -o "UserKnownHostsFile /dev/null" \
     -o "StrictHostKeyChecking no" \
-    scalerunner@$IP -t "bash -c 'sudo singularity shell -e instance://i'"
+    scalerunner@$IP ${SINGULARITY_CMD}
