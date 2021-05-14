893295561e842df73326d2d2f4e34b7c9325a89b
Adam Olech <aolech@antmicro.com>
2021-05-14 15:17:24 +0000

    Create a stub of RunnerVersionHandler

---
+++ src/Runner.Common/RunnerVersionHandler.cs
@@ -0,0 +1,14 @@
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+
+namespace GitHub.Runner.Common
+{
+    public sealed class RunnerVersionHandler
+    {
+        public string GetVersion()
+        {
+            return "";
+        }
+    }
+}
