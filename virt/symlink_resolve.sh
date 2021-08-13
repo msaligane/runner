ae73084b688dd72cfb52f508bfcca8b1c6d839af
Adam Olech <aolech@antmicro.com>
2021-08-13 11:50:55 +0000

    virt/symlink_resolve: improve output

--- virt/symlink_resolve.sh
+++ virt/symlink_resolve.sh
@@ -6,19 +6,24 @@ cd $(dirname $0)
 
 PREFIX=$1
 IP_PREFIX=`hostname`
+SSH_HOST=$IP_PREFIX-auto-spawned$PREFIX
 
 sshSend() {
+    echo "# connecting to $SSH_HOST"
     echo "+ $@"
     /usr/bin/ssh -q \
         -o "UserKnownHostsFile /dev/null" \
         -o "StrictHostKeyChecking no" \
         -o "ServerAliveInterval 10" \
-        scalerunner@$IP_PREFIX-auto-spawned$PREFIX << EOF
+        -o "ConnectTimeout 10" \
+        -o "ConnectionAttempts 3" \
+        scalerunner@$SSH_HOST << EOF
 sudo -s
 $@
 EOF
 }
 
+echo "# `getent hosts $SSH_HOST`"
 sshSend "mkdir -p /root/work"
 sshSend "mount --bind /mnt/2/work /root/work"
 sshSend 'cd /root/work;for f in $(find -type l);do cp -f $(readlink $f) $f || true;done;'
